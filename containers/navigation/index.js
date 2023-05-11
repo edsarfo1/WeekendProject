82af3e77a75bfcdf6089b9bb66727412556df5c0
Edward Sarfo <edsarfo1@gmail.com>
2023-05-11 11:14:27 +0100

    redux

--- containers/navigation/index.js
+++ containers/navigation/index.js
@@ -1,38 +1,43 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Provider} from 'react-redux';
+
 import WelcomeScreen from '../../screens/welcome';
 import Login from '../../screens/login';
 import Dashboard from '../../screens/dashboard';
 import SignUp from '../../screens/signup';
+import {store} from '../../src/store';
 
 const Stack = createNativeStackNavigator();
 
 function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
-        <Stack.Screen
-          name="Welcome"
-          component={WelcomeScreen}
-          options={{title: 'Welcome'}}
-        />
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{title: 'Login'}}
-        />
-        <Stack.Screen
-          name="Signup"
-          component={SignUp}
-          options={{title: 'Signup'}}
-        />
-        <Stack.Screen
-          name="Dashboard"
-          component={Dashboard}
-          options={{title: 'Dashboard'}}
-        />
-      </Stack.Navigator>
+      <Provider store={store}>
+        <Stack.Navigator initialRouteName="Welcome">
+          <Stack.Screen
+            name="Welcome"
+            component={WelcomeScreen}
+            options={{title: 'Welcome'}}
+          />
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{title: 'Login'}}
+          />
+          <Stack.Screen
+            name="Signup"
+            component={SignUp}
+            options={{title: 'Signup'}}
+          />
+          <Stack.Screen
+            name="Dashboard"
+            component={Dashboard}
+            options={{title: 'Dashboard'}}
+          />
+        </Stack.Navigator>
+      </Provider>
     </NavigationContainer>
   );
 }
